139893b3bf972a55c75f197ffb42d6efbf2054b8
Valentin Bonani <valentin.bonani@gmail.com>
2020-10-05 21:11:17 -0300

    Finished first phase, usable for testing

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f5324836b9c22c4960b568afa9f40351'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2885f865-6975-45fd-9133-8c5db2a45f64'
+};
